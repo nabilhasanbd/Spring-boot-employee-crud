2e046f9e8bd246e2bd3848241fe8720a152012f7
Nabil <nabil.uiu.bd@gmail.com>
2023-03-18 01:20:21 +0600

    Added service layer

---
+++ src/main/java/com/example/Employee/Service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.example.Employee.Service;
+
+import com.example.Employee.Model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee saveEmployee(Employee employee);
+    List<Employee> getAllEmployees();
+    Employee getEmployeeById(long id);
+    Employee updateEmployee(Employee employee, long id);
+    void deleteEmployee(long id);
+}
