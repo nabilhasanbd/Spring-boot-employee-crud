4811ada8c1273cbb92cea2a503a6b3929a9d3271
Nabil <nabil.uiu.bd@gmail.com>
2023-03-19 17:12:53 +0600

    Added annotation

--- src/main/java/com/example/Employee/Exception/ResourceNotFoundExceptions.java
+++ src/main/java/com/example/Employee/Exception/ResourceNotFoundExceptions.java
@@ -1,6 +1,14 @@
 package com.example.Employee.Exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serial;
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundExceptions extends RuntimeException{
+
+    @Serial
+    private static final long serialVersionUID = 1L;
     private String resourceName ;
     private String fieldName ;
     private Object fieldValue;
