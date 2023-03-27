97f0a0c367990a3b585b0b9cdf37e1fa2f2d53e3
Nabil <nabil.uiu.bd@gmail.com>
2023-03-27 23:28:46 +0600

    Added hibernate validator dependency

--- src/main/java/com/example/Employee/Model/Employee.java
+++ src/main/java/com/example/Employee/Model/Employee.java
@@ -6,6 +6,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 
 import com.sun.istack.NotNull;
 import lombok.Data;
@@ -20,13 +22,16 @@ public class Employee {
     private long id;
 
     @NotNull
+    @Size(min=2 , message = "First name character must be more one")
     @Column(name = "first_name", nullable = false)
     private String firstName;
 
     @NotNull
+    @Size(min=2 , message = "First name character must be more one")
     @Column(name = "last_name")
     private String lastName;
 
+    @Email
     @Column(name = "email")
     private String email;
 }
