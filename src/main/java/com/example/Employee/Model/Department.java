1bf826c08ea1c627ad9223fde6dc77eddd9473a4
Nabil <nabil.uiu.bd@gmail.com>
2023-03-29 17:19:39 +0600

    Department class added

---
+++ src/main/java/com/example/Employee/Model/Department.java
@@ -0,0 +1,9 @@
+package com.example.Employee.Model;
+
+public class Department {
+
+    private long id;
+    private String departmentName;
+    private String getDepartmentID;
+    private String getDepartmentType;
+}
