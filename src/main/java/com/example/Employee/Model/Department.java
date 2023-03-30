064b3eb66780be732b813c0b87ff39cb0c847b04
Nabil <nabil.uiu.bd@gmail.com>
2023-03-31 01:22:00 +0600

    Added validation

--- src/main/java/com/example/Employee/Model/Department.java
+++ src/main/java/com/example/Employee/Model/Department.java
@@ -1,9 +1,27 @@
 package com.example.Employee.Model;
 
+import com.sun.istack.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Data
+@Entity
+@Table(name = "department")
 public class Department {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    @NotNull
+    @Size(min = 3, message = "Department name must be at least 3 digit")
     private String departmentName;
+
+    @NotNull
     private String getDepartmentID;
+
     private String getDepartmentType;
 }
